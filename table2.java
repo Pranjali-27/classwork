2ce555e13ddf6eb9c1172e8ed4ccdc685f16b5a9
Pranjali-27 <106953217+Pranjali-27@users.noreply.github.com>
2022-06-30 03:01:36 -0700

    Add files via upload

---
+++ table2.java
@@ -0,0 +1,18 @@
+class table2
+{
+public static void main(String[] args)
+{
+  int i=1;
+while(i<=10)
+{
+int j=1;
+while(j<=5)
+{
+System.out.print("\t" +i*j+" ");
+j++;
+}
+System.out.println();
+i++;
+}
+}
+}
